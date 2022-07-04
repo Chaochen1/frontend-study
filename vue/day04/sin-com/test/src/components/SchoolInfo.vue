b2fac1dad7192e8dbef27799ad26234595d261b4
chenchao <chenchao@nj.iscas.ac.cn>
2022-07-04 14:31:08 +0800

    [ADD]:vue-数据监测|指令|过滤器|生命周期|组件

---
+++ vue/day04/sin-com/test/src/components/SchoolInfo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{schoolName}}</h2>
+        <h2>学校地址：{{schoolAddr}}</h2>
+        <StudentInfo></StudentInfo>
+    </div>
+</template>
+
+
+<script>
+    import StudentInfo from './StudentInfo.vue'
+    export default {
+    name: "SchoolInfo",
+    data() {
+        return {
+            schoolName: "hehai",
+            schoolAddr: "nanjing"
+        };
+    },
+    comments: {
+        StudentInfo
+    },
+    components: { StudentInfo }
+}
+</script>
+
+
+<style>
+
+    .demo{
+        background-color: skyblue;
+    }
+
+</style>
---
+++ vue/day04/sin-com/test/src/components/StudentInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="dd">
+        <h2>学生姓名：{{stu.name}}</h2>
+        <h2>学生年龄：{{stu.age}}</h2>
+    </div>
+</template>
+
+
+
+<script>
+
+export default{
+    name:'StudentInfo',
+    data() {
+        return {
+            stu:{
+                name:'cc',
+                age:28
+            }
+        }
+    },
+}
+</script>
+
+
+<style>
+    .dd{
+        background-color: red;
+    }
+
+</style>
